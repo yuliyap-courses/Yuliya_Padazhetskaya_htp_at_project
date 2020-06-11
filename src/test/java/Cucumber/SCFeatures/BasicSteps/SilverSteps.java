005de9793b4681b49364abd44088d402f9098fe9
yuliya_padazhetskaya <yufiya@yandex.ru>
2020-06-11 23:36:04 +0300

    Added FyndBy annotation Create SilverStep for login

--- src/test/java/Cucumber/SCFeatures/BasicSteps/SilverSteps.java
+++ src/test/java/Cucumber/SCFeatures/BasicSteps/SilverSteps.java
@@ -1,14 +1,39 @@
 package Cucumber.SCFeatures.BasicSteps;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class SilverSteps {
-    String loginContainer= "//*[@class=['sc-cugefK ewcnPt']";
-String login = "//*[contains(text(), '%email')]";
-String password = "//*[contains(text(), '%password')]";
-String button = "//*[contains(text(), '%Войти')]";
 
-public void openLoginContainer(String login, String password){
+public void CheckPage (WebDriver driver){
+    PageFactory.initElements(driver, this);
+}
+@FindBy(xpath = "//*[@class=/sc-fyjhYU eVJmYW/]")
+WebElement loginContainer;
 
+@FindBy(xpath ="//*[@type= /email/]")
+    WebElement loginField;
 
+@FindBy(xpath = "//*[@type= /password/]")
+WebElement passwordField;
+
+@FindBy(xpath = "//*[text()= /Войти/]")
+    WebElement loginButton;
+
+public  void EnterEmailToLoginField(String email){
+    loginField.sendKeys(email);
+}
+public  void EnterPasswordToPasswordField(String password){
+    passwordField.sendKeys(password);
+}
+
+public void SubmitByButton(boolean value){
+    if(!value) loginButton.click();
+    try { Thread.sleep(3000);}
+    catch (InterruptedException e) {}
+}
 
 }
-}
+
