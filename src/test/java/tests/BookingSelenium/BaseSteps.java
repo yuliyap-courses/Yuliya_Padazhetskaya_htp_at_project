0d3462c642f7539397da3b76ac1c765e04335f4b
padazhetskayayuliya <yufiya@yandex.ru>
2020-07-20 10:47:45 +0300

    Test commit to verify Git

--- src/test/java/tests/BookingSelenium/BaseSteps.java
+++ src/test/java/tests/BookingSelenium/BaseSteps.java
@@ -3,8 +3,26 @@ package tests.BookingSelenium;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 
 public class BaseSteps {
+@FindBy(xpath = "[//*[@id='ss']")
+    WebElement citySearchField;
+//enter city
+// click perform
+
+@FindBy(xpath = "//*[@id='xp__guests__toggle']/span[2]/span[1]")
+    WebElement  satellitesSearchSelector;
+
+@FindBy(xpath = "//*[@id='//*class='sb-searchbox__button']")
+    WebElement submitSearchButton;
+
+         public void CitySearchField(WebElement citySearchField) {
+        citySearchField.sendKeys();
+
+
+
 
         public static void FindElement(By xpath, WebDriver driver){
             WebElement SomeElement = driver.findElement(By.xpath(String.valueOf(xpath)));
